77dc54823ab27025721710929b4f0f875fc60e99
ZH-Matthew <m.zhitenev23@gmail.com>
2023-08-03 14:01:58 +0300

    HW_2_15 Проект клонирован для работы с Integer Реализовано тестирование трех вариантов сортировки Лучший показатель выявлен у сортировки "Вставкой" Реализован бинарный поиск Переопределен Contains под сортировку вставкой и бинарный поиск Проект покрыт тестами

--- src/test/java/org/example/Constants.java
+++ src/test/java/org/example/Constants.java
@@ -9,4 +9,13 @@ public class Constants {
     public static String ST_3 = "А я тесты пишу...";
     public static String ST_L = "Я тут лишний";
 
+    public static Integer INT_1 = 10;
+    public static Integer INT_2 = 20;
+    public static Integer INT_3 = 30;
+    public static Integer INT_L = 444;
+
+    public static Integer[] ARR_1 = {INT_1,INT_2,INT_3};
+
+    public static Integer[] ARR_2 = {INT_3,INT_2,INT_1};
+
 }
